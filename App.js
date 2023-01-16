fab8cbad6540d1070911909db1c0476ea974d755
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-16 14:54:57 -0300

    feat: Component Field #02

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Alert} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import params from './src/params'
 import Field from './src/components/Field';
 
 export default class App extends Component {
-
   render() {
     return (
       <View style={styles.container}>
